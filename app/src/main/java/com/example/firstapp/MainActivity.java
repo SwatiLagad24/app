f36530bea5002a53dfa9d2442831247eef5a77fb
Swati <swatilagad24@gmail.com>
2022-06-09 11:59:08 +0530

    Ass1_TextView_With_BackGroundColor

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends Activity {
     private TextView txtColorPanelTwo;
     private TextView txtColorPanelThree;
     private TextView txtColorPanelFour;
-    private TextView ;
     private Button btnChangeColor;
 
     @Override
